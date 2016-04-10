1ff046d8a68b548e3dc2e25ba7693fc4c0e13cad
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-04-10 16:53:02 -0600

    Update copyright info

--- src/jquery-doubleTap.js
+++ src/jquery-doubleTap.js
@@ -4,7 +4,7 @@
  * 
  * Custom jQuery event method that triggers a callback after double-tapping/double-clicking on the selected DOM element
  * 
- * Copyright (c) 2015-2016 Dan Zervoudakes
+ * Copyright (c) 2015, 2016 Dan Zervoudakes
  * Released under the MIT license
  * https://github.com/dzervoudakes/jQuery-doubleTap/blob/master/LICENSE
  */
